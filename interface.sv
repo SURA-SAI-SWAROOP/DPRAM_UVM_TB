47e6acd2f61c8ee5115575ff87caedd143ccee59
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2025-01-16 13:34:17 +0530

    interface.sv

--- interface.sv
+++ interface.sv
@@ -1,18 +1,29 @@
 interface my_if (input clk, reset);
-  logic enable;
+  logic we;
+  logic re;
+  logic[3:0]waddr;
+  logic[3:0]raddr;
   logic [7:0] data_in;
-  logic [3:0]addr;
   logic [7:0] data_out;
   
   clocking DRV@(posedge clk);
     default input #1 output #1;
-    output enable, data_in, addr;
+    output we;
+    output re;
+    output waddr;
+    output raddr;
+    output data_in;
     input data_out;
   endclocking
     
   clocking MON@(posedge clk);
     default input #1 output #1;
-    input enable, data_in, addr, data_out;
+    input we;
+    input re;
+    input waddr;
+    input raddr;
+    input data_in;
+    input data_out;
     input reset;
   endclocking
     
