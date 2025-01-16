e13ed117743ec10c2c707effaa50e5c245729d3e
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2025-01-16 13:50:25 +0530

    my_agent.sv

--- my_agent.sv
+++ my_agent.sv
@@ -1,5 +1,5 @@
 class my_agent extends uvm_agent;
-  `uvm_component_utils (my_agent)
+  `uvm_component_utils(my_agent)
   
   my_monitor mon;
   my_driver drv;
@@ -9,15 +9,15 @@ class my_agent extends uvm_agent;
     super.new(name,parent);
   endfunction
     
-  virtual function void build_phase (uvm_phase phase);
-    super.build_phase (phase);
-    mon=my_monitor::type_id::create("mon", this);
-    drv=my_driver::type_id::create("drv", this);
-    sqr=my_sequencer::type_id::create("sqr", this);
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon=my_monitor::type_id::create("mon",this);
+    drv=my_driver::type_id::create("drv",this);
+    sqr=my_sequencer::type_id::create("sqr",this);
   endfunction
     
-  virtual function void connect_phase (uvm_phase phase);
-    super.connect_phase (phase);
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
     drv.seq_item_port.connect(sqr.seq_item_export);
   endfunction
 endclass
